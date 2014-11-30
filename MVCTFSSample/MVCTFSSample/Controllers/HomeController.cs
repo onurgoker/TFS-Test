225ce367f10c2e9bbf33d5454d661f7ed6266cd8
Onur GÖKER <onurgoker89@gmail.com>
2014-11-30 19:24:31 +0200

    My first commit
    İlk check in yapıldı.

---
+++ MVCTFSSample/MVCTFSSample/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCTFSSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Çok Özel Bir Mesaj.";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Uygulama Açıklama Sayfası.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "İletişim Sayfası.";
+
+            return View();
+        }
+    }
+}
